6de6c1420c3a1ec9f64570e732156048b4003c7a
sndekoning <sylvester.de.koning90@gmail.com>
2016-02-23 18:31:06 +0100

    further additions

--- datareshaping.R
+++ datareshaping.R
@@ -10,9 +10,12 @@ stdata_corr <- stdata_corr[-ind_sum,]
 ## Creating a new data frame with only the variables to be used.
 stdata_corr <- select(stdata_corr, EVTYPE, FATALITIES, INJURIES, PROPDMG, PROPDMGEXP,
                     CROPDMG, CROPDMGEXP)
-    
-stdata_pubdmg <- stdata_corr %>% select(EVTYPE, FATALITIES, INJURIES) %>%
-    filter(FATALITIES != 0 | INJURIES != 0)
+
+## Public health data table with a total variable added and zero damages removed.    
+stdata_pubdmg <- stdata_corr %>% 
+    select(EVTYPE, FATALITIES, INJURIES) %>%
+    filter(FATALITIES != 0 | INJURIES != 0) %>%
+    mutate(TOTAL = FATALITIES + INJURIES)
 
 ## Function to transform the exponential variables.
 exp_transform <- function(damages, exponential){
@@ -23,24 +26,30 @@ exp_transform <- function(damages, exponential){
         damages * 1000
     }
     else if(exponential == "M"){
-        damages * 1E6
+        damages * 1E+06
     }
     else if(exponential == "B"){
-        damages * 1E9
+        damages * 1E+09
     }
 }
 
-inv_exp <- c(0:9, "-", "+", "?")
+## Subsetting the variables to be used, and dropping the zero-damage entries.
 stdata_ecodmg <- stdata_corr %>% 
     select(EVTYPE, PROPDMG, PROPDMGEXP, CROPDMG, CROPDMGEXP) %>%
     filter(PROPDMG != 0 | CROPDMG != 0)
  
+## Dropping the non-standard exponential variables.
 stdata_ecodmg <- stdata_ecodmg %>%
     filter(PROPDMGEXP == "" | PROPDMGEXP == "K" | PROPDMGEXP == "M" | PROPDMGEXP == "B") %>% 
     filter(CROPDMGEXP == "" | CROPDMGEXP == "K" | CROPDMGEXP == "M" | CROPDMGEXP == "B")
 
-stdata_ecodmg <-mutate(stdata_ecodmg, 
-                        PROPDMG_TOT = exp_transform(stdata_ecodmg$PROPDMG, stdata_ecodmg$PROPDMGEXP), 
-                        CROPDMG_TOT = exp_transform(stdata_ecodmg$CROPDMG, stdata_ecodmg$CROPDMGEXP)
-                        )
+## Broken code. Prop mutates, Crop doesnt.
+## stdata_ecodmg <- mutate(stdata_ecodmg, 
+##                        PROPDMG_TOT = exp_transform(stdata_ecodmg$PROPDMG, stdata_ecodmg$PROPDMGEXP))
+## stdata_ecodmg <- mutate(stdata_ecodmg, 
+##                        CROPDMG_TOT = exp_transform(stdata_ecodmg$CROPDMG, stdata_ecodmg$CROPDMGEXP))
+
+## Proper code to apply the exponentials to the damages variables.
+stdata_ecodmg$CROPDMG_TOT <- mapply(exp_transform, stdata_ecodmg$CROPDMG, stdata_ecodmg$CROPDMGEXP)
+stdata_ecodmg$PROPDMG_TOT <- mapply(exp_transform, stdata_ecodmg$PROPDMG, stdata_ecodmg$PROPDMGEXP)          
 
---
+++ plots.R
@@ -0,0 +1,19 @@
+library(ggplot2)
+
+##top_fatal <- stdata_pubdmg %>%
+##  arrange(desc(FATALITIES)) %>%
+##  top_n(50, FATALITIES)
+
+##top_injuries <- stdata_pubdmg %>%
+##    arrange(desc(INJURIES)) %>%
+##    top_n(50, INJURIES)
+
+top_total <- stdata_pubdmg %>%
+   arrange(desc(TOTAL)) %>%
+    top_n(50, TOTAL)
+
+p <- ggplot(data = top_total, aes(EVTYPE, TOTAL))
+p + geom_bar(stat = "identity") + 
+    labs(title = "Total Public Health Damage", x = "Weather Event", 
+                                        y = "Total injured or killed")
+    
